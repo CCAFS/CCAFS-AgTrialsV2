39b71a43b50363b51e2d5accc14df89620c942a0
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-10 13:23:47 -0500

    Correccion Errores

--- lib/vendor/symfony/lib/plugins/sfPropelPlugin/data/generator/sfPropelModule/admin/skeleton/actions/actions.class.php
+++ lib/vendor/symfony/lib/plugins/sfPropelPlugin/data/generator/sfPropelModule/admin/skeleton/actions/actions.class.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once dirname(__FILE__).'/../lib/##MODULE_NAME##GeneratorConfiguration.class.php';
-require_once dirname(__FILE__).'/../lib/##MODULE_NAME##GeneratorHelper.class.php';
+require_once dirname(__FILE__) . '/../lib/##MODULE_NAME##GeneratorConfiguration.class.php';
+require_once dirname(__FILE__) . '/../lib/##MODULE_NAME##GeneratorHelper.class.php';
 
 /**
  * ##MODULE_NAME## actions.
@@ -12,5 +12,4 @@ require_once dirname(__FILE__).'/../lib/##MODULE_NAME##GeneratorHelper.class.php
  * @version    SVN: $Id: actions.class.php 12474 2008-10-31 10:41:27Z fabien $
  */
 class ##MODULE_NAME##Actions extends auto##UC_MODULE_NAME##Actions
-{
-}
+{}
