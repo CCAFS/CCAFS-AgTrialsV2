fde2a862d226bbe1e143ea4d480feb459c365403
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-12 09:20:11 -0500

    Mejorando Estilo V2

--- apps/agtrials/modules/admin/templates/batchuploadanotherSuccess.php
+++ apps/agtrials/modules/admin/templates/batchuploadanotherSuccess.php
@@ -1,8 +1,6 @@
 <div style="margin-top: 10px;">
     <span class="Title">Batch upload another</span>
 </div>
-
 <div class="Session" style="margin-top: 10px; margin-bottom: 10px; border-bottom-width: 0px; padding: 10px; border-top-width: 10px;">
-
-    
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 </div>       
--- apps/agtrials/modules/home/templates/contactSuccess.php
+++ apps/agtrials/modules/home/templates/contactSuccess.php
@@ -1,1 +1,6 @@
-  
+<div style="margin-top: 10px;">
+    <span class="Title">Contact</span>
+</div>
+<div class="Session" style="margin-top: 10px; margin-bottom: 10px; border-bottom-width: 0px; padding: 10px; border-top-width: 10px;">
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+</div>        
