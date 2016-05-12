adf19f5df503ee5787f82ab4768ecbdd78ddd574
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-12 08:35:04 -0500

    Mejorando Estilo V2

---
+++ apps/agtrials/modules/admin/actions/actions.class.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ *  This file is part of AgTrials
+ *
+ *  AgTrials is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  at your option) any later version.
+ *
+ *  AgTrials is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with DMSP.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2012 (C) Climate Change, Agriculture and Food Security (CCAFS)
+ * 
+ * Created on : OCT - 2014
+ * @author    :  Herlin R. Espinosa G. - herlin25@gmail.com
+ * @version   :  ~
+ */
+
+include("../lib/functions/function.php");
+include("../lib/PHPMailer/PHPMailer.php");
+
+/**
+ * home actions.
+ *
+ * @package    trialsites
+ * @subpackage home
+ * @author     Herlin R. Espinosa G. - CIAT-DAPA
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class adminActions extends sfActions {
+
+    /**
+     * Executes Batchuploadanother action
+     *
+     * @param sfRequest $request A request object
+     */
+    public function executeBatchuploadanother(sfWebRequest $request) {
+        
+    }
+}
---
+++ apps/agtrials/modules/admin/templates/batchuploadanotherSuccess.php
@@ -0,0 +1,8 @@
+<div style="margin-top: 10px;">
+    <span class="Title">Batch upload another</span>
+</div>
+
+<div class="Session" style="margin-top: 10px; margin-bottom: 10px; border-bottom-width: 0px; padding: 10px; border-top-width: 10px;">
+
+    
+</div>       
