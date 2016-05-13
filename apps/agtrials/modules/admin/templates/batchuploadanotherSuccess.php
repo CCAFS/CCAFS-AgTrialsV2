80b9c5d87d5b5d6b9b346ef8bd361e860e311551
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-13 10:57:30 -0500

    Mejorando Estilo V2

--- apps/agtrials/modules/admin/templates/batchuploadanotherSuccess.php
+++ apps/agtrials/modules/admin/templates/batchuploadanotherSuccess.php
@@ -9,8 +9,29 @@
     </div>
     <div class="col-md-9 sf_admin_form" style="margin-top: 13px; width: 930px;">
         <span class="Title">Batch upload another</span>
-        <div class="Session"style="margin-top: 10px; margin-bottom: 10px;">
-            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <div class="Session" style="margin-top: 10px; margin-bottom: 10px;">
+            <div class="ContainerBatch">
+                <div>Download templates:</div>
+                <div class="row RowDownloadTemplate">
+                    <div class="DownloadTemplate" onclick="window.location.href = '/downloadestrutureproject'">
+                        <img width="60" height="50" border="0" src="/images/DownloadTemplate.png">
+                        <div>Trial Project Template</div>
+                    </div>
+                    <div class="DownloadTemplate" onclick="window.location.href = '/downloadcheckbatchtriallocation'">
+                        <img width="60" height="50" border="0" src="/images/DownloadTemplate.png">
+                        <div>Trial Location Template</div>
+                    </div>
+                    <div class="DownloadTemplate" onclick="window.location.href = '/downloadestruturevariablesmeasured'">
+                        <img width="60" height="50" border="0" src="/images/DownloadTemplate.png">
+                        <div>Trial Varieties Template</div>
+                    </div>
+                    <div class="DownloadTemplate" onclick="window.location.href = '/downloadestruturevariety'">
+                        <img width="60" height="50" border="0" src="/images/DownloadTemplate.png">
+                        <div>Trial Variables Measured Template</div>
+                    </div>
+                </div>
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            </div>
         </div>
     </div>
 </div>
