47dc9bfcb41ec4328d4254a07de9e028923ea0cd
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-16 08:26:33 -0500

    Mejorando Estilo V2

--- plugins/sfDoctrineGuardPlugin/modules/sfGuardAuth/templates/forgotpasswordSuccess.php
+++ plugins/sfDoctrineGuardPlugin/modules/sfGuardAuth/templates/forgotpasswordSuccess.php
@@ -1,16 +1,16 @@
 <script>
-    $(document).ready(function() {
+    $(document).ready(function () {
         //DEFINICION DE CAMPOS OBLIGATORIOS
         var CamposObligatorios = {
             'emailaddress': 'Email address'
         };
 
         //inicio: VALIDAMOS EL ENVIO DEL FORMULARIO
-        $("#SubmitForgotpassword").click(function(event) {
+        $("#SubmitForgotpassword").click(function (event) {
             var Ico = "<img src='/images/bullet-black-icon.png'> ";
             var BanderaFaltantes = false;
             var MensajeFaltantes = "";
-            $.each(CamposObligatorios, function(Id, Campo) {
+            $.each(CamposObligatorios, function (Id, Campo) {
                 if ($('#' + Id).attr('value') == '') {
                     BanderaFaltantes = true;
                     MensajeFaltantes += "&ensp;&ensp;&ensp; " + Ico + Campo + " \n";
@@ -30,7 +30,7 @@
             }
         });
 
-        $('#emailaddress').blur(function() {
+        $('#emailaddress').blur(function () {
             if ($('#emailaddress').attr('value') != '' && ($("#emailaddress").val().indexOf('@', 0) == -1 || $("#emailaddress").val().indexOf('.', 0) == -1)) {
                 $('#emailaddress').attr('value', '');
                 jError('Email address', 'Error', null);
@@ -39,8 +39,8 @@
 
     });
 </script>
-<div class="page-header">
-    <h1 class="title-module">Forgot Password</h1>
+<div style="margin-top: 10px;">
+    <span class="Title">Forgot Password</span>
 </div>
 <?php if (isset($Notice)) { ?>
     <span>
@@ -53,16 +53,18 @@
     <?php echo image_tag('loading.gif'); ?>
     <br>Please Wait...
 </div>
-<form class="form-horizontal" id="FormForgotpassword" name="FormForgotpassword" action="<?php echo url_for('@forgotpassword'); ?>" enctype="multipart/form-data" method="post" autocomplete="off">
-    <fieldset>
-        <div class="form-group control-type-text">
-            <label class="col-sm-4 control-label">Email address</label>      
-            <div class=" col-sm-4 control-type-text">
-                <input class="form-control" type="text" name="emailaddress" id="emailaddress"/>                           
+<div class="Session" style="margin-top: 10px; margin-bottom: 10px; border-bottom-width: 0px; padding: 10px; border-top-width: 10px;">
+    <form class="form-horizontal" id="FormForgotpassword" name="FormForgotpassword" action="<?php echo url_for('@forgotpassword'); ?>" enctype="multipart/form-data" method="post" autocomplete="off">
+        <fieldset>
+            <div class="form-group control-type-text">
+                <div class="col-sm-2">Email address:</div>
+                <div class=" col-sm-4 control-type-text">
+                    <input class="form-control" type="text" name="emailaddress" id="emailaddress"/>                           
+                </div>
             </div>
+        </fieldset>
+        <div>
+            <button class="btn btn-action" type="button" title=" Submit " id="SubmitForgotpassword" neme="SubmitForgotpassword"><span class="glyphicon glyphicon-saved" aria-hidden="true"></span>&ensp;Submit&ensp;</button>
         </div>
-    </fieldset>
-    <div class="form-actions">
-        <button class="btn btn-action" type="button" title=" Submit " id="SubmitForgotpassword" neme="SubmitForgotpassword"><span class="glyphicon glyphicon-saved" aria-hidden="true"></span>&ensp;Submit&ensp;</button>
-    </div>
-</form>
+    </form>
+</div>
