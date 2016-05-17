d14b62d6c2d861e08fec871e7daede63e1ebd342
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-17 08:27:13 -0500

    Mejorando Estilo V2

---
+++ apps/agtrials/modules/admin/templates/_ProsessesCheckMenu.php
@@ -0,0 +1,9 @@
+<div class="MenuTrials">
+    <div onclick="window.location.href = '/batchuploadanother'" class="MenuTrialsButton"> Batch upload another </div>
+    <div onclick="window.location.href = '#'" class="MenuTrialsButton selected"> Prosesses Check
+        <ul class="subMenu">
+            <li><a class="page-scroll" href="/checktriallocation">Trial location</a></li>
+            <li><a class="page-scroll" href="/checkbatchtriallocation">Batch trial location</a></li>
+        </ul>
+    </div>
+</div>
