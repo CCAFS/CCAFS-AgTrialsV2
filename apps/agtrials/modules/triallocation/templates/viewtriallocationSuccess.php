07a926852121f4fed883df96d539a702f0b1b247
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-17 09:49:10 -0500

    Mejorando Estilo V2

--- apps/agtrials/modules/triallocation/templates/viewtriallocationSuccess.php
+++ apps/agtrials/modules/triallocation/templates/viewtriallocationSuccess.php
@@ -5,124 +5,123 @@
 <link rel="stylesheet" type="text/css" media="screen" href="/jquery.alerts/jquery.alerts.css" />
 <link rel="stylesheet" type="text/css" media="screen" href="/css/bootstrap-personalized.css" />
 <link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" />
-<div class="page-header">
-    <h1 class="title-module">View Trial location</h1>
-</div>
-<br>
-<fieldset>
-    <div class="row form-group control-type-text" style="margin-bottom: 0px;">
-        <div class="col-sm-5 form-group control-type-text">
-            <label class="col-sm-3 control-label">Id</label>
-            <div class="col-sm-6 control-type-text">
-                <?php echo $TbTriallocation[0]['id_triallocation']; ?>
+<span class="Title">Check Trial Location</span>
+<div class="Session" style="margin-top: 10px; margin-bottom: 10px;">
+    <fieldset>
+        <div class="row form-group control-type-text" style="margin-bottom: 0px;">
+            <div class="col-sm-5 form-group control-type-text">
+                <div class="col-sm-3">Id:</div>
+                <div class="col-sm-6 control-type-text">
+                    <?php echo $TbTriallocation[0]['id_triallocation']; ?>
+                </div>
+            </div>
+            <div class="col-sm-6 form-group control-type-text">
+                <div class="col-sm-3">Name:</div>
+                <div class="col-sm-8 control-type-text">
+                    <?php echo $TbTriallocation[0]['trlcname']; ?>
+                </div>
             </div>
         </div>
-        <div class="col-sm-6 form-group control-type-text">
-            <label class="col-sm-3 control-label">Name</label>
-            <div class="col-sm-8 control-type-text">
-                <?php echo $TbTriallocation[0]['trlcname']; ?>
+        <div class="row form-group control-type-text" style="margin-bottom: 0px;">
+            <div class="col-sm-5 form-group control-type-text">
+                <div class="col-sm-3">Latitude:</div>
+                <div class="col-sm-6 control-type-text">
+                    <?php echo round($TbTriallocation[0]['trlclatitude'], 4) ?>
+                </div>
             </div>
-        </div>
-    </div>
-    <div class="row form-group control-type-text" style="margin-bottom: 0px;">
-        <div class="col-sm-5 form-group control-type-text">
-            <label class="col-sm-3 control-label">Latitude</label>
-            <div class="col-sm-6 control-type-text">
-                <?php echo round($TbTriallocation[0]['trlclatitude'], 4) ?>
+            <div class="col-sm-6 form-group control-type-text">
+                <div class="col-sm-3">Longitude:</div>
+                <div class="col-sm-8 control-type-text">
+                    <?php echo round($TbTriallocation[0]['trlclongitude'], 4) ?>
+                </div>
             </div>
-        </div>
-        <div class="col-sm-6 form-group control-type-text">
-            <label class="col-sm-3 control-label">Longitude</label>
-            <div class="col-sm-8 control-type-text">
-                <?php echo round($TbTriallocation[0]['trlclongitude'], 4) ?>
+        </div>  
+        <div class="row form-group control-type-text" style="margin-bottom: 0px;">
+            <div class="col-sm-5 form-group control-type-text">
+                <div class="col-sm-3">Altitude:</div>
+                <div class="col-sm-6 control-type-text">
+                    <?php echo round($TbTriallocation[0]['trlcaltitude'], 4) ?>
+                </div>
             </div>
-        </div>
-    </div>  
-    <div class="row form-group control-type-text" style="margin-bottom: 0px;">
-        <div class="col-sm-5 form-group control-type-text">
-            <label class="col-sm-3 control-label">Altitude</label>
-            <div class="col-sm-6 control-type-text">
-                <?php echo round($TbTriallocation[0]['trlcaltitude'], 4) ?>
+            <div class="col-sm-6 form-group control-type-text">
+                <div class="col-sm-3">Country:</div>
+                <div class="col-sm-8 control-type-text">
+                    <?php echo $TbTriallocation[0]['country']; ?>
+                </div>
             </div>
-        </div>
-        <div class="col-sm-6 form-group control-type-text">
-            <label class="col-sm-3 control-label">Country</label>
-            <div class="col-sm-8 control-type-text">
-                <?php echo $TbTriallocation[0]['country']; ?>
+        </div> 
+        <div class="row form-group control-type-text" style="margin-bottom: 0px;">
+            <div class="col-sm-5 form-group control-type-text">
+                <div class="col-sm-3">District:</div>
+                <div class="col-sm-6 control-type-text">
+                    <?php echo $TbTriallocation[0]['district']; ?>
+                </div>
             </div>
-        </div>
-    </div> 
-    <div class="row form-group control-type-text" style="margin-bottom: 0px;">
-        <div class="col-sm-5 form-group control-type-text">
-            <label class="col-sm-3 control-label">District</label>
-            <div class="col-sm-6 control-type-text">
-                <?php echo $TbTriallocation[0]['district']; ?>
+            <div class="col-sm-6 form-group control-type-text">
+                <div class="col-sm-3">Sub-district:</div>
+                <div class="col-sm-8 control-type-text">
+                    <?php echo $TbTriallocation[0]['subdistrict']; ?>
+                </div>
             </div>
-        </div>
-        <div class="col-sm-6 form-group control-type-text">
-            <label class="col-sm-3 control-label">Sub-district</label>
-            <div class="col-sm-8 control-type-text">
-                <?php echo $TbTriallocation[0]['subdistrict']; ?>
+        </div> 
+        <div class="row form-group control-type-text" style="margin-bottom: 0px;">
+            <div class="col-sm-5 form-group control-type-text">
+                <div class="col-sm-3">Village:</div>
+                <div class="col-sm-6 control-type-text">
+                    <?php $TbTriallocation[0]['village']; ?>
+                </div>
             </div>
-        </div>
-    </div> 
-    <div class="row form-group control-type-text" style="margin-bottom: 0px;">
-        <div class="col-sm-5 form-group control-type-text">
-            <label class="col-sm-3 control-label">Village</label>
-            <div class="col-sm-6 control-type-text">
-                <?php $TbTriallocation[0]['village']; ?>
-            </div>
-        </div>
-    </div> 
+        </div> 
 
-    <div class="row form-group control-type-text" style="margin-left: 15px;">
-        <head>
-            <script type="text/javascript" src="/js/jquery-ui-1.11.3/jquery-1.11.3.min.js"></script>
-            <script src="https://maps.googleapis.com/maps/api/js?v=3.20"></script>
-        </head>
-        <body onunload="GUnload()">
-            <div id="map" style="width: 855px; height: 330px"></div>
-            <script type="text/javascript">
+        <div class="row form-group control-type-text" style="margin-left: 15px;">
+            <head>
+                <script type="text/javascript" src="/js/jquery-ui-1.11.3/jquery-1.11.3.min.js"></script>
+                <script src="https://maps.googleapis.com/maps/api/js?v=3.20"></script>
+            </head>
+            <body onunload="GUnload()">
+                <div id="map" style="width: 855px; height: 330px"></div>
+                <script type="text/javascript">
 
-                var i_lat = <?php echo round($TbTriallocation[0]['trlclatitude'], 4); ?>;
-                var i_lon = <?php echo round($TbTriallocation[0]['trlclongitude'], 4); ?>;
+                    var i_lat = <?php echo round($TbTriallocation[0]['trlclatitude'], 4); ?>;
+                    var i_lon = <?php echo round($TbTriallocation[0]['trlclongitude'], 4); ?>;
 
-                var map = null;
-                var infoWindow = null;
+                    var map = null;
+                    var infoWindow = null;
 
-                function openInfoWindow(marker) {
-                    var markerLatLng = marker.getPosition();
-                    infoWindow.setContent(["<div>Latitud: " + markerLatLng.lat() + "<br>Longitud: " + markerLatLng.lng()].join(''));
-                    infoWindow.open(map, marker);
-                }
-
-                function initialize() {
-                    var myLatlng = new google.maps.LatLng(i_lat, i_lon);
-                    var myOptions = {
-                        zoom: 14,
-                        center: myLatlng,
-                        mapTypeId: google.maps.MapTypeId.HYBRID
+                    function openInfoWindow(marker) {
+                        var markerLatLng = marker.getPosition();
+                        infoWindow.setContent(["<div>Latitud: " + markerLatLng.lat() + "<br>Longitud: " + markerLatLng.lng()].join(''));
+                        infoWindow.open(map, marker);
                     }
 
-                    map = new google.maps.Map($("#map").get(0), myOptions);
+                    function initialize() {
+                        var myLatlng = new google.maps.LatLng(i_lat, i_lon);
+                        var myOptions = {
+                            zoom: 14,
+                            center: myLatlng,
+                            mapTypeId: google.maps.MapTypeId.HYBRID
+                        }
 
-                    infoWindow = new google.maps.InfoWindow();
+                        map = new google.maps.Map($("#map").get(0), myOptions);
 
-                    var marker = new google.maps.Marker({
-                        position: myLatlng,
-                        draggable: true,
-                        map: map,
-                        title: "Info"
+                        infoWindow = new google.maps.InfoWindow();
+
+                        var marker = new google.maps.Marker({
+                            position: myLatlng,
+                            draggable: true,
+                            map: map,
+                            title: "Info"
+                        });
+
+                        google.maps.event.addListener(marker, 'click', function () {
+                            openInfoWindow(marker);
+                        });
+                    }
+                    $(document).ready(function () {
+                        initialize();
                     });
-
-                    google.maps.event.addListener(marker, 'click', function() {
-                        openInfoWindow(marker);
-                    });
-                }
-                $(document).ready(function() {
-                    initialize();
-                });
-            </script>
-        </body>
-    </div>
-</fieldset>
+                </script>
+            </body>
+        </div>
+    </fieldset>    
+</div>
