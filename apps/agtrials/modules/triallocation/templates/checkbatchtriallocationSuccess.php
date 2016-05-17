2e0fdea2ed93ecde949b0b5f228b527daa52f73b
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-17 08:01:54 -0500

    Mejorando Estilo V2

--- apps/agtrials/modules/triallocation/templates/checkbatchtriallocationSuccess.php
+++ apps/agtrials/modules/triallocation/templates/checkbatchtriallocationSuccess.php
@@ -1,6 +1,6 @@
 <script>
-    $(document).ready(function() {
-        $('#uploadsubmit').click(function() {
+    $(document).ready(function () {
+        $('#uploadsubmit').click(function () {
             var id_country = $('#id_country').attr('value');
             var checkbatchtriallocationfile = $('#checkbatchtriallocationfile').attr('value');
             if (id_country == '') {
@@ -21,43 +21,59 @@
         });
     });
 </script>
-<div class="page-header">
-    <h1 class="title-module">Check batch trial location</h1>
+<div class="row">
+    <div class="col-md-2 left-column">
+        <div class="MenuTrials">
+            <div onclick="window.location.href = '/batchuploadanother'" class="MenuTrialsButton"> Batch upload another </div>
+            <div onclick="window.location.href = '#'" class="MenuTrialsButton selected"> Prosesses Check
+                <ul class="subMenu">
+                    <li><a class="page-scroll" href="/checktriallocation">Trial location</a></li>
+                    <li><a class="page-scroll" href="/checkbatchtriallocation">Batch trial location</a></li>
+                </ul>
+            </div>
+            <div onclick="window.location.href = '/batchuploadtrials'" class="MenuTrialsButton"> Batch Upload Trials</div>            
+        </div>
+    </div>
+    <div class="col-md-10 sf_admin_form" style="margin-top: 13px;">
+        <span class="Title">Check batch trial location</span>
+        <div class="Session" style="margin-top: 10px; margin-bottom: 10px;">
+            <form class="form-horizontal" id="checkbatchtriallocation" name="checkbatchtriallocation" action="<?php echo url_for('@checkbatchtriallocation'); ?>" enctype="multipart/form-data" method="post">
+                <fieldset>
+                    <legend align= "left">&ensp;<b>Attention</b>&ensp;</legend>
+                    <span><?php echo image_tag('attention-icon.png'); ?> Check trial location template file must have <b>.xls</b> extension and must be smaller than <b>5 MB</b> maximum size </span></br>
+                    <span><?php echo image_tag('attention-icon.png'); ?> Exact number of columns <b>'1'</b> for Template File</span></br>
+                    <span><?php echo image_tag('attention-icon.png'); ?> Max. <b>50000 Records</b> for Template File </span></br>
+                    <span><?php echo image_tag('attention-icon.png'); ?> Don't close the window during the process </span>
+                </fieldset>
+                <br>
+                <fieldset>
+                    <legend align= "left">&ensp;<b>Download Template</b>&ensp;</legend>
+                    <button class="btn btn-action" type="button" name="Downloadcheckbatchtriallocation" id="Downloadcheckbatchtriallocation"  onclick="window.location.href = '/downloadcheckbatchtriallocation'" title="Check trial location template file"><span aria-hidden="true" class="glyphicon glyphicon-download"></span>&ensp;Check trial location template file</button>      
+                </fieldset>
+                <br>
+                <br>
+                <fieldset>
+                    <legend align= "left">&ensp;<b>Upload Template</b>&ensp;</legend>
+                    <div class="form-group control-type-text">
+                        <label class="col-sm-5 control-label" style="width: 260px;">Country</label>
+                        <div class="col-sm-4 control-type-text">
+                            <?php echo select_from_country_triallocation("id_country", null, "class='form-control'"); ?>
+                        </div>
+                    </div>
+                    <div class="form-group control-type-text">
+                        <label class="col-sm-5 control-label" style="width: 260px;">Check trial location template file</label>
+                        <div class="col-sm-4 control-type-text">
+                            <input type="file" name="checkbatchtriallocationfile" id="checkbatchtriallocationfile">
+                        </div>
+                    </div>
+                </fieldset>
+                <br>
+                <fieldset>
+                    <div class="form-group control-type-text" style="margin-left: 0px; margin-right: 0px;">
+                        <button class="btn btn-action" type="button" title=" Execute " id="uploadsubmit" neme="Execute"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span>&ensp;Execute&ensp;</button>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    </div>
 </div>
-<form class="form-horizontal" id="checkbatchtriallocation" name="checkbatchtriallocation" action="<?php echo url_for('@checkbatchtriallocation'); ?>" enctype="multipart/form-data" method="post">
-    <fieldset>
-        <legend align= "left">&ensp;<b>Attention</b>&ensp;</legend>
-        <span><?php echo image_tag('attention-icon.png'); ?> Check trial location template file must have <b>.xls</b> extension and must be smaller than <b>5 MB</b> maximum size </span></br>
-        <span><?php echo image_tag('attention-icon.png'); ?> Exact number of columns <b>'1'</b> for Template File</span></br>
-        <span><?php echo image_tag('attention-icon.png'); ?> Max. <b>50000 Records</b> for Template File </span></br>
-        <span><?php echo image_tag('attention-icon.png'); ?> Don't close the window during the process </span>
-    </fieldset>
-    <br>
-    <fieldset>
-        <legend align= "left">&ensp;<b>Download Template</b>&ensp;</legend>
-        <button class="btn btn-action" type="button" name="Downloadcheckbatchtriallocation" id="Downloadcheckbatchtriallocation"  onclick="window.location.href = '/downloadcheckbatchtriallocation'" title="Check trial location template file"><span aria-hidden="true" class="glyphicon glyphicon-download"></span>&ensp;Check trial location template file</button>      
-    </fieldset>
-    <br>
-    <br>
-    <fieldset>
-        <legend align= "left">&ensp;<b>Upload Template</b>&ensp;</legend>
-        <div class="form-group control-type-text">
-            <label class="col-sm-5 control-label" style="width: 260px;">Country</label>
-            <div class="col-sm-4 control-type-text">
-                <?php echo select_from_country_triallocation("id_country", null, "class='form-control'"); ?>
-            </div>
-        </div>
-        <div class="form-group control-type-text">
-            <label class="col-sm-5 control-label" style="width: 260px;">Check trial location template file</label>
-            <div class="col-sm-4 control-type-text">
-                <input type="file" name="checkbatchtriallocationfile" id="checkbatchtriallocationfile">
-            </div>
-        </div>
-    </fieldset>
-    <br>
-    <fieldset>
-        <div class="form-group control-type-text" style="margin-left: 0px; margin-right: 0px;">
-            <button class="btn btn-action" type="button" title=" Execute " id="uploadsubmit" neme="Execute"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span>&ensp;Execute&ensp;</button>
-        </div>
-    </fieldset>
-</form>
--- apps/agtrials/modules/triallocation/templates/checktriallocationSuccess.php
+++ apps/agtrials/modules/triallocation/templates/checktriallocationSuccess.php
@@ -67,7 +67,8 @@
             <div onclick="window.location.href = '/batchuploadanother'" class="MenuTrialsButton"> Batch upload another </div>
             <div onclick="window.location.href = '#'" class="MenuTrialsButton selected"> Prosesses Check
                 <ul class="subMenu">
-                    <li><a class="page-scroll" href="/checktriallocation">Check Trial location</a></li>
+                    <li><a class="page-scroll" href="/checktriallocation">Trial location</a></li>
+                    <li><a class="page-scroll" href="/checkbatchtriallocation">Batch trial location</a></li>
                 </ul>
             </div>
             <div onclick="window.location.href = '/batchuploadtrials'" class="MenuTrialsButton"> Batch Upload Trials</div>            
