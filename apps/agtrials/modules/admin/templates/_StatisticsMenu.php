e6d45c3ead5f6bef06ac98e5d0ef0b736530b0b9
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-18 08:34:12 -0500

    Mejorando Estilo V2

---
+++ apps/agtrials/modules/admin/templates/_StatisticsMenu.php
@@ -0,0 +1,7 @@
+<div class="MenuTrials">
+    <div onclick="window.location.href = '/#'" class="MenuButtonLeft">By Technology</div>
+    <div onclick="window.location.href = '/#'" class="MenuButtonLeft">By Country</div>
+    <div onclick="window.location.href = '/#'" class="MenuButtonLeft">By Institution</div>
+    <div onclick="window.location.href = '/#'" class="MenuButtonLeft">By Trial Group</div>
+    <div onclick="window.location.href = '/#'" class="MenuButtonLeft">By Trial Site</div>
+</div> 
--- apps/agtrials/modules/home/templates/statisticsSuccess.php
+++ apps/agtrials/modules/home/templates/statisticsSuccess.php
@@ -1,6 +1,13 @@
-<div style="margin-top: 10px;">
-    <span class="Title">Statistics</span>
+<div class="row">
+    <div class="col-md-2 left-column">
+        <?php include_partial('admin/StatisticsMenu') ?>
+    </div>
+    <div class="col-md-9 sf_admin_form" style="margin-top: 13px; width: 930px;">
+        <span class="Title">Statistics</span>
+        <div class="Session" style="margin-top: 10px; margin-bottom: 10px;">
+            <div class="container">
+                888888888888
+            </div>
+        </div>
+    </div>
 </div>
-<div class="Session" style="margin-top: 10px; margin-bottom: 10px; border-bottom-width: 0px; padding: 10px; border-top-width: 10px;">
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-</div>       
