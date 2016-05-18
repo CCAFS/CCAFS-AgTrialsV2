9c3b09c26c300f1f1c77f90155dc0fb4ccf4df0a
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-18 08:06:31 -0500

    Mejorando Estilo V2

--- apps/agtrials/modules/trial/templates/_form_actions.php
+++ apps/agtrials/modules/trial/templates/_form_actions.php
@@ -5,11 +5,13 @@
             <?php echo $helper->linkToSave($form->getObject(), array('params' => 'class= fg-button ui-state-default fg-button-icon-left ', 'class_suffix' => 'save', 'label' => 'Save', 'ui-icon' => '',)) ?>
         </div>
     <?php } else if (sfContext::getInstance()->getRequest()->getParameterHolder()->get('action') == 'show') { ?>
-        <?php echo $helper->linkToDelete($form->getObject(), array('params' => 'class= fg-button ui-state-default fg-button-icon-left ', 'confirm' => 'Are you sure?', 'class_suffix' => 'delete', 'label' => 'Delete', 'ui-icon' => '',)) ?>
-        <div class="btn-group">
-            <?php echo $helper->linkToNew(array('params' => 'class= fg-button ui-state-default  ', 'class_suffix' => 'new', 'label' => 'New',)) ?>
-            <?php echo $helper->linkToEdit($form->getObject(), array('params' => 'class= fg-button ui-state-default  ', 'class_suffix' => 'edit', 'label' => 'Edit',)) ?>
-        </div>
+        <?php if ($sf_user->isAuthenticated()) { ?>
+            <?php echo $helper->linkToDelete($form->getObject(), array('params' => 'class= fg-button ui-state-default fg-button-icon-left ', 'confirm' => 'Are you sure?', 'class_suffix' => 'delete', 'label' => 'Delete', 'ui-icon' => '',)) ?>
+            <div class="btn-group">
+                <?php echo $helper->linkToNew(array('params' => 'class= fg-button ui-state-default  ', 'class_suffix' => 'new', 'label' => 'New',)) ?>
+                <?php echo $helper->linkToEdit($form->getObject(), array('params' => 'class= fg-button ui-state-default  ', 'class_suffix' => 'edit', 'label' => 'Edit',)) ?>
+            </div>
+        <?php } ?>
     <?php } else { ?>
         <?php echo $helper->linkToDelete($form->getObject(), array('params' => 'class= fg-button ui-state-default fg-button-icon-left ', 'confirm' => 'Are you sure?', 'class_suffix' => 'delete', 'label' => 'Delete', 'ui-icon' => '',)) ?>
         <div class="btn-group">
