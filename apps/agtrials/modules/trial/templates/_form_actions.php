c488e64f199b020ffd2b348f6654d7b1e6b886cd
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-18 08:02:04 -0500

    Mejorando Estilo V2

--- apps/agtrials/modules/trial/templates/_form_actions.php
+++ apps/agtrials/modules/trial/templates/_form_actions.php
@@ -1,16 +1,20 @@
 <div class="form-actions">
-    <?php if ($form->isNew()): ?>
+    <?php if (sfContext::getInstance()->getRequest()->getParameterHolder()->get('action') == 'new') { ?>
         <?php echo $helper->linkToDelete($form->getObject(), array('params' => 'class= fg-button ui-state-default fg-button-icon-left ', 'confirm' => 'Are you sure?', 'class_suffix' => 'delete', 'label' => 'Delete', 'ui-icon' => '',)) ?>
         <div class="btn-group">
-            <?php echo $helper->linkToList(array('params' => 'class= fg-button ui-state-default fg-button-icon-left ', 'class_suffix' => 'list', 'label' => 'Back to list', 'ui-icon' => '',)) ?>
             <?php echo $helper->linkToSave($form->getObject(), array('params' => 'class= fg-button ui-state-default fg-button-icon-left ', 'class_suffix' => 'save', 'label' => 'Save', 'ui-icon' => '',)) ?>
         </div>
-    <?php else: ?>
+    <?php } else if (sfContext::getInstance()->getRequest()->getParameterHolder()->get('action') == 'show') { ?>
         <?php echo $helper->linkToDelete($form->getObject(), array('params' => 'class= fg-button ui-state-default fg-button-icon-left ', 'confirm' => 'Are you sure?', 'class_suffix' => 'delete', 'label' => 'Delete', 'ui-icon' => '',)) ?>
         <div class="btn-group">
-            <?php echo $helper->linkToList(array('params' => 'class= fg-button ui-state-default fg-button-icon-left ', 'class_suffix' => 'list', 'label' => 'Back to list', 'ui-icon' => '',)) ?>
+            <?php echo $helper->linkToNew(array('params' => 'class= fg-button ui-state-default  ', 'class_suffix' => 'new', 'label' => 'New',)) ?>
+            <?php echo $helper->linkToEdit($form->getObject(), array('params' => 'class= fg-button ui-state-default  ', 'class_suffix' => 'edit', 'label' => 'Edit',)) ?>
+        </div>
+    <?php } else { ?>
+        <?php echo $helper->linkToDelete($form->getObject(), array('params' => 'class= fg-button ui-state-default fg-button-icon-left ', 'confirm' => 'Are you sure?', 'class_suffix' => 'delete', 'label' => 'Delete', 'ui-icon' => '',)) ?>
+        <div class="btn-group">
             <?php echo $helper->linkToSave($form->getObject(), array('params' => 'class= fg-button ui-state-default fg-button-icon-left ', 'class_suffix' => 'save', 'label' => 'Save', 'ui-icon' => '',)) ?>
             <?php echo $helper->linkToNew(array('params' => 'class= fg-button ui-state-default  ', 'class_suffix' => 'new', 'label' => 'New',)) ?>
         </div>
-    <?php endif; ?>
+    <?php } ?>
 </div>
