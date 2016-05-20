c9c6ede7bcdf54ac8a0d79aa9b2dad21ebaee35f
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-20 10:50:23 -0500

    Mejorando Estilo V2

--- apps/agtrials/modules/home/templates/contactSuccess.php
+++ apps/agtrials/modules/home/templates/contactSuccess.php
@@ -1,6 +1,133 @@
+<script>
+    $(document).ready(function () {
+        //DEFINICION DE CAMPOS OBLIGATORIOS
+        var CamposObligatorios = {
+            'firstname': 'First name',
+            'lastname': 'Last name',
+            'emailaddress': 'Email',
+            'telephone': 'Telephone',
+            'country': 'Country',
+            'message': 'Message',
+            'code': 'Security Code'
+        };
+
+        $('#SubmitContact').click(function () {
+            var Ico = "<img src='/images/bullet-black-icon.png'> ";
+            var BanderaFaltantes = false;
+            var MensajeFaltantes = "";
+            $.each(CamposObligatorios, function (Id, Campo) {
+                if ($('#' + Id).attr('value') == '') {
+                    BanderaFaltantes = true;
+                    MensajeFaltantes += "&ensp;&ensp;&ensp; " + Ico + Campo + " \n";
+                    CampoObligatorio(Id);
+                } else {
+                    CampoNormalObligatorio(Id);
+                }
+            });
+
+            //VERIFICACION MENSAJE DE ALERTA
+            if (BanderaFaltantes) {
+                Mensaje(MensajeFaltantes);
+                event.preventDefault();
+            } else {
+                $('#div_loading').show();
+                $('#FormContact').submit();
+            }
+        });
+
+        $('#refreshcode').click(function () {
+            $.ajax({
+                type: "GET",
+                url: "/home/refreshcode",
+                success: function (data) {
+                    $('#securitycode').attr('value', data);
+                }
+            });
+
+        });
+
+        $('#code').blur(function () {
+            var code = $('#code').attr('value');
+            var securitycode = $('#securitycode').attr('value');
+            if (code != securitycode) {
+                $('#code_error').html("Sorry, the code you entered was invalid");
+                $('#code').attr('value', '');
+            } else {
+                $('#code_error').html("");
+            }
+        });
+
+        $('#code').focus(function () {
+            $('#code_error').html('');
+        });
+
+    });
+</script>
 <div style="margin-top: 10px;">
-    <span class="Title">Contact</span>
+    <span class="Title">Contact Us</span>
+</div>
+<?php if (isset($Notice)) { ?>
+    <span>
+        <div class="alert alert-success">
+            <?php echo $Notice; ?>
+        </div>
+    </span>
+<?php } ?>
+<div id="div_loading" class="loading" align="center" style="display:none;">
+    <?php echo image_tag('loading.gif'); ?>
+    <br>Please Wait...
 </div>
 <div class="Session" style="margin-top: 10px; margin-bottom: 10px; border-bottom-width: 0px; padding: 10px; border-top-width: 10px;">
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <form class="form-horizontal"  id="FormContact" name="FormContact" action="" method="post"  autocomplete="off">
+        <div class="form-group control-type-text">
+            <div class="col-sm-2">First name:</div>
+            <div class="col-sm-4 control-type-text">
+                <input class="form-control" type="text" name="firstname" id="firstname"/>
+            </div>
+        </div>
+        <div class="form-group control-type-text">
+            <div class="col-sm-2">Last name:</div>
+            <div class="col-sm-4 control-type-text">
+                <input class="form-control" type="text" name="lastname" id="lastname"/>
+            </div>
+        </div>
+        <div class="form-group control-type-text">
+            <div class="col-sm-2">E-mail:</div>
+            <div class="col-sm-4 control-type-text">
+                <input class="form-control" type="text" name="emailaddress" id="emailaddress"/>
+            </div>
+        </div>
+        <div class="form-group control-type-text">
+            <div class="col-sm-2">Telephone:</div>
+            <div class="col-sm-4 control-type-text">
+                <input class="form-control" type="text" name="telephone" id="telephone"/>
+            </div>
+        </div>
+        <div class="form-group control-type-text">
+            <div class="col-sm-2">Country:</div>
+            <div class="col-sm-4 control-type-text">
+                <input class="form-control" type="text" name="country" id="country"/>
+            </div>
+        </div>
+        <div class="form-group control-type-text">
+            <div class="col-sm-2">Message:</div>
+            <div class="col-sm-4 control-type-text">
+                <textarea class="form-control" id="message" name="message" title="Message" rows="5" cols="50"></textarea>
+            </div>
+        </div>
+        <div class="form-group control-type-text">
+            <div class="col-sm-2">Security Code:</div>
+            <div class="col-sm-4 control-type-text">
+                <div id="CodeError"></div>
+                <div class="col-sm-6 control-type-text" style="padding-left: 0px;">
+                    <input class="form-control" type="text" name="code" id="code" size="15" oncopy="return false;" onpaste="return false;" oncut="return false;" autocomplete="off" />
+                </div>
+                <input type="text" value="<?php echo @generatecode(6); ?>" name="securitycode" id="securitycode" class="CodeBox" oncopy="return false;" onpaste="return false;" oncut="return false;" readonly="readonly" /> 
+                <span><img src="/images/actions-view-refresh-icon.png" id="RefreshCode" title="Refresh security code" alt="picture not loaded"/></span>
+            </div>
+        </div>
+        <div>
+            <button class="btn btn-action" type="button" title=" Submit " id="SubmitContact" neme="SubmitContact"><span class="glyphicon glyphicon-saved" aria-hidden="true"></span>&ensp;Submit&ensp;</button>
+        </div>
+    </form>
 </div>        
