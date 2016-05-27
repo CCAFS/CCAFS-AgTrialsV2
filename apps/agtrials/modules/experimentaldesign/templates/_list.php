73a8dd9d167ff95330ec190d2d8fec1bb5344157
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-27 16:19:00 -0500

    Mejorando Estilo V2

---
+++ apps/agtrials/modules/experimentaldesign/templates/_list.php
@@ -0,0 +1,53 @@
+<?php if (!$pager->getNbResults()): ?>
+    <p class="alert alert-warning"><?php echo __('No result', array(), 'sf_admin') ?></p>
+    <div style="display:none;" class="btn-toolbar">
+        <?php include_partial('experimentaldesign/list_actions', array('helper' => $helper)) ?>
+    </div>
+<?php else: ?>
+
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <?php include_partial('experimentaldesign/list_th', array('sort' => $sort)) ?>
+                <th class="LabelList LabelListAction"><?php echo __('Actions', array(), 'sf_admin') ?></th>
+            </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <th colspan="5">
+                    <div class="pull-right">
+                        <div class="TotalResults">Records: <span id="CountResults" class="badge"><?php echo $pager->getNbResults(); ?></span></div>
+                    </div>
+                    <?php if ($pager->haveToPaginate()): ?>
+                        <?php include_partial('experimentaldesign/pagination', array('pager' => $pager)) ?>
+                    <?php endif; ?>
+                    <div class="btn-toolbar">
+                        <?php include_partial('experimentaldesign/list_batch_actions', array('helper' => $helper)) ?>
+                    </div>
+                </th>
+            </tr>
+        </tfoot>
+        <tbody>
+            <?php foreach ($pager->getResults() as $tb_experimentaldesign): ?>
+                <tr>
+                    <?php include_partial('experimentaldesign/list_td', array('tb_experimentaldesign' => $tb_experimentaldesign)) ?>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php endif; ?>
+
+<script type="text/javascript">
+    /* <![CDATA[ */
+    function toggleCheckboxes()
+    {
+        var $mainCheckbox = $('#checkAll');
+        $boxes = $('tbody input[type="checkbox"]');
+
+        if ($mainCheckbox.is(':checked'))
+            $boxes.attr('checked', 'checked');
+        else
+            $boxes.removeAttr('checked');
+    }
+    /* ]]> */
+</script>
---
+++ apps/agtrials/modules/experimentaldesign/templates/newSuccess.php
@@ -0,0 +1,6 @@
+<?php use_helper('I18N', 'Date') ?>
+<?php include_partial('experimentaldesign/assets') ?>
+<?php include_partial('experimentaldesign/flashes') ?>
+<?php include_partial('experimentaldesign/form_header', array('tb_experimentaldesign' => $tb_experimentaldesign, 'form' => $form, 'configuration' => $configuration)) ?>
+<?php include_partial('experimentaldesign/form', array('tb_experimentaldesign' => $tb_experimentaldesign, 'form' => $form, 'configuration' => $configuration, 'helper' => $helper)) ?>
+<?php include_partial('experimentaldesign/form_footer', array('tb_experimentaldesign' => $tb_experimentaldesign, 'form' => $form, 'configuration' => $configuration)) ?>
