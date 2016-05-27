9db32ada530e4a6e9cdfbf8ae6cc252d72db8eb7
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-27 10:50:14 -0500

    Mejorando Estilo V2

--- web/GoogleMapsV3/GoogleMapsV3.js
+++ web/GoogleMapsV3/GoogleMapsV3.js
@@ -52,10 +52,10 @@ function toggleMarkerManager() {
         }
     } else {
         mgr = new MarkerManager(map, {
-            trackMarkers: true, 
+            trackMarkers: true,
             maxZoom: 15
         });
-        google.maps.event.addListener(mgr, 'loaded', function() {
+        google.maps.event.addListener(mgr, 'loaded', function () {
             mgr.addMarkers(markers.info, 0, 5);
             mgr.refresh();
         });
@@ -83,18 +83,18 @@ function toggleMarkerClusterer() {
  * Initializes the map and listeners.
  */
 function initialize() {
-    
+
     map = new google.maps.Map(document.getElementById('map'), {
         center: new google.maps.LatLng(0, 0),
         zoom: 2,
         mapTypeId: google.maps.MapTypeId.TERRAIN
     });
-    
+
     google.maps.event.addDomListener(document.getElementById('mc-cb'), 'click', toggleMarkerClusterer);
     google.maps.event.addDomListener(document.getElementById('mgr-cb'), 'click', toggleMarkerManager);
 
     var infowindow = new google.maps.InfoWindow();
-   
+
     if (markers) {
         for (var level in markers) {
             for (var i = 0; i < markers[level].length; i++) {
@@ -103,7 +103,7 @@ function initialize() {
                 var Log = details.log;
                 var Title = details.title;
                 var Context = details.context;
-                
+
                 markers[level][i] = new google.maps.Marker({
                     title: Title,
                     context: Context,
@@ -113,17 +113,16 @@ function initialize() {
                     flat: true,
                     icon: '/images/GoogleMap/m0.png'
                 });
-                google.maps.event.addListener(markers[level][i], 'click', (function(marker, i) {
-                    return function() {
+                google.maps.event.addListener(markers[level][i], 'click', (function (marker, i) {
+                    return function () {
                         var details = markers[level][i];
                         infowindow.setContent(details.context);
                         infowindow.open(map, markers[level][i]);
                     }
-                })(markers[level][i], i));    
+                })(markers[level][i], i));
 
             }
         }
     }
 }
-
 google.maps.event.addDomListener(window, 'load', initialize);
