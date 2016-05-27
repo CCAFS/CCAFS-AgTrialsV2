66c84f9cf7b4988212f760e369fd50125e39f6ae
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-27 15:37:57 -0500

    Mejorando Estilo V2

---
+++ apps/agtrials/modules/contactperson/templates/editSuccess.php
@@ -0,0 +1,6 @@
+<?php use_helper('I18N', 'Date') ?>
+<?php include_partial('contactperson/assets') ?>
+  <?php include_partial('contactperson/flashes') ?>
+  <?php include_partial('contactperson/form_header', array('tb_contactperson' => $tb_contactperson, 'form' => $form, 'configuration' => $configuration)) ?>
+  <?php include_partial('contactperson/form', array('tb_contactperson' => $tb_contactperson, 'form' => $form, 'configuration' => $configuration, 'helper' => $helper)) ?>
+  <?php include_partial('contactperson/form_footer', array('tb_contactperson' => $tb_contactperson, 'form' => $form, 'configuration' => $configuration)) ?>
