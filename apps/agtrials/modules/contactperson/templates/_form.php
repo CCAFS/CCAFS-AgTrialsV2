459cd00dfea2906fc911f986c0be134548615601
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-27 16:10:13 -0500

    Mejorando Estilo V2

--- apps/agtrials/modules/contactperson/templates/_form.php
+++ apps/agtrials/modules/contactperson/templates/_form.php
@@ -8,17 +8,17 @@
     </div>
     <div class="col-md-10 sf_admin_form" style="margin-top: 13px;">
         <span class="Title">Contact person</span>
-        <form id="FormContactperson" class="form-horizontal" action="/contactperson" method="post" id="FormContactperson" name="FormContactperson" enctype="multipart/form-data">
-            <div class="Session" style="margin-top: 10px; margin-bottom: 10px;">
-                <?php echo $form->renderHiddenFields(false) ?>
-                <?php if ($form->hasGlobalErrors()): ?>
-                    <?php echo $form->renderGlobalErrors() ?>
-                <?php endif; ?> 
-                <?php foreach ($configuration->getFormFields($form, $form->isNew() ? 'new' : 'edit') as $fieldset => $fields): ?>
-                    <?php include_partial('contactperson/form_fieldset', array('tb_contactperson' => $tb_contactperson, 'form' => $form, 'fields' => $fields, 'fieldset' => $fieldset)) ?>
-                <?php endforeach; ?>
-            </div>
-            <?php include_partial('contactperson/form_actions', array('tb_contactperson' => $tb_contactperson, 'form' => $form, 'configuration' => $configuration, 'helper' => $helper)) ?>
+        <?php echo form_tag_for($form, '@tb_contactperson', array('class' => 'form-horizontal', 'id' => 'FormContactperson', 'enctype' => 'multipart/form-data')) ?>
+        <div class="Session" style="margin-top: 10px; margin-bottom: 10px;">
+            <?php echo $form->renderHiddenFields(false) ?>
+            <?php if ($form->hasGlobalErrors()): ?>
+                <?php echo $form->renderGlobalErrors() ?>
+            <?php endif; ?> 
+            <?php foreach ($configuration->getFormFields($form, $form->isNew() ? 'new' : 'edit') as $fieldset => $fields): ?>
+                <?php include_partial('contactperson/form_fieldset', array('tb_contactperson' => $tb_contactperson, 'form' => $form, 'fields' => $fields, 'fieldset' => $fieldset)) ?>
+            <?php endforeach; ?>
+        </div>
+        <?php include_partial('contactperson/form_actions', array('tb_contactperson' => $tb_contactperson, 'form' => $form, 'configuration' => $configuration, 'helper' => $helper)) ?>
         </form>
     </div>
 </div>
