47d2768727a053cf9dfaeeebf371bd52a7a489d1
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-31 10:48:06 -0500

    Mejorando Estilo V2

---
+++ apps/agtrials/modules/institution/templates/_filters.php
@@ -0,0 +1,36 @@
+<?php use_stylesheets_for_form($form) ?>
+<?php use_javascripts_for_form($form) ?>
+
+<div id="filterPopup" class="modal fade">
+    <form action="<?php echo url_for('tb_institution_collection', array('action' => 'filter')) ?>" method="post" class="form-horizontal">
+        <span class="Title">Institution</span>
+        <div class="Session" style="margin-top: 10px; margin-bottom: 10px;">
+            <div class="modal-body">
+                <?php if ($form->hasGlobalErrors()): ?>
+                    <?php echo $form->renderGlobalErrors() ?>
+                <?php endif; ?>
+
+                <?php foreach ($configuration->getFormFilterFields($form) as $name => $field): ?>
+                    <?php if ((isset($form[$name]) && $form[$name]->isHidden()) || (!isset($form[$name]) && $field->isReal())) continue ?>
+                    <?php
+                    include_partial('institution/filters_field', array(
+                        'name' => $name,
+                        'attributes' => $field->getConfig('attributes', array()),
+                        'label' => $field->getConfig('label'),
+                        'help' => $field->getConfig('help'),
+                        'form' => $form,
+                        'field' => $field,
+                        'class' => 'control-type-' . strtolower($field->getType()) . ' control-name-' . $name,
+                    ))
+                    ?>
+                <?php endforeach; ?>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="submit" class="btn btn-action"><?php echo __('<span class="glyphicon glyphicon-search" aria-hidden="true"></span> Filter', array(), 'sf_admin') ?></button>
+            <?php echo link_to(__('<span class="glyphicon glyphicon-erase" aria-hidden="true"></span> Reset', array(), 'sf_admin'), 'tb_institution_collection', array('action' => 'filter'), array('query_string' => '_reset', 'method' => 'post', 'class' => 'btn btn-action')) ?>
+            <a class="btn btn-action" class="close" data-dismiss="modal"><span aria-hidden="true" class="glyphicon glyphicon-remove"></span> Close</a>
+        </div>
+        <?php echo $form->renderHiddenFields() ?>
+    </form>
+</div>
---
+++ apps/agtrials/modules/traitclass/templates/_filters_field.php
@@ -0,0 +1,16 @@
+<?php if ($field->isPartial()): ?>
+    <?php include_partial('traitclass/' . $name, array('type' => 'filter', 'form' => $form, 'attributes' => $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes)) ?>
+<?php elseif ($field->isComponent()): ?>
+    <?php include_component('traitclass', $name, array('type' => 'filter', 'form' => $form, 'attributes' => $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes)) ?>
+<?php else: ?>
+    <div class="form-group <?php echo $class ?>">
+        <div class="col-sm-3"><?php echo $label; ?>:</div>
+        <div class="col-sm-6 <?php echo $class ?>">
+            <?php echo $form[$name]->renderError() ?>
+            <?php echo $form[$name]->render(array('class' => 'form-control')) ?>
+            <?php if ($help || $help = $form[$name]->renderHelp()): ?>
+                <p class="help-block"><?php echo __($help, array(), 'messages') ?></p>
+            <?php endif; ?>
+        </div>
+    </div>
+<?php endif; ?>
