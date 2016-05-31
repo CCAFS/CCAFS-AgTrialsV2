efbeb9481171ba390ce8f097ef0c8c2eacc5286d
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-05-31 11:32:35 -0500

    Mejorando Estilo V2

--- apps/agtrials/modules/admin/templates/_ModuleMenu.php
+++ apps/agtrials/modules/admin/templates/_ModuleMenu.php
@@ -1,13 +1,38 @@
+<?php
+$Modulo = sfContext::getInstance()->getRequest()->getParameterHolder()->get('module');
+if ($Modulo == 'contactperson')
+    $Selectedcontactperson = "selected";
+if ($Modulo == 'crop')
+    $Selectedcrop = "selected";
+if ($Modulo == 'donor')
+    $Selecteddonor = "selected";
+if ($Modulo == 'experimentaldesign')
+    $Selectedexperimentaldesign = "selected";
+if ($Modulo == 'institution')
+    $Selectedinstitution = "selected";
+if ($Modulo == 'project')
+    $Selectedproject = "selected";
+if ($Modulo == 'rolecontactperson')
+    $Selectedrolecontactperson = "selected";
+if ($Modulo == 'traitclass')
+    $Selectedtraitclass = "selected";
+if ($Modulo == 'triallocation')
+    $Selectedtriallocation = "selected";
+if ($Modulo == 'variablesmeasured')
+    $Selectedvariablesmeasured = "selected";
+if ($Modulo == 'variety')
+    $Selectedvariety = "selected";
+?>
 <div class="MenuTrials">
-    <div onclick="window.location.href = '/contactperson'" class="MenuButtonLeft"> Contact person </div>
-    <div onclick="window.location.href = '/crop'" class="MenuButtonLeft"> Crop </div>
-    <div onclick="window.location.href = '/donor'" class="MenuButtonLeft"> Donor </div>
-    <div onclick="window.location.href = '/experimentaldesign'" class="MenuButtonLeft"> Experimental design </div>
-    <div onclick="window.location.href = '/institution'" class="MenuButtonLeft"> Institution </div>
-    <div onclick="window.location.href = '/project'" class="MenuButtonLeft"> Project </div>
-    <div onclick="window.location.href = '/rolecontactperson'" class="MenuButtonLeft"> Role contact person </div>
-    <div onclick="window.location.href = '/traitclass'" class="MenuButtonLeft"> Trait class </div>
-    <div onclick="window.location.href = '/triallocation'" class="MenuButtonLeft"> Trial location </div>
-    <div onclick="window.location.href = '/variablesmeasured'" class="MenuButtonLeft"> Variables measured </div>
-    <div onclick="window.location.href = '/variety'" class="MenuButtonLeft"> Variety </div>
+    <div onclick="window.location.href = '/contactperson'" class="MenuButtonLeft <?php echo $Selectedcontactperson; ?>"> Contact person </div>
+    <div onclick="window.location.href = '/crop'" class="MenuButtonLeft <?php echo $Selectedcrop; ?>"> Crop </div>
+    <div onclick="window.location.href = '/donor'" class="MenuButtonLeft <?php echo $Selecteddonor; ?>"> Donor </div>
+    <div onclick="window.location.href = '/experimentaldesign'" class="MenuButtonLeft <?php echo $Selectedexperimentaldesign; ?>"> Experimental design </div>
+    <div onclick="window.location.href = '/institution'" class="MenuButtonLeft <?php echo $Selectedinstitution; ?>"> Institution </div>
+    <div onclick="window.location.href = '/project'" class="MenuButtonLeft <?php echo $Selectedproject; ?>"> Project </div>
+    <div onclick="window.location.href = '/rolecontactperson'" class="MenuButtonLeft <?php echo $Selectedrolecontactperson; ?>"> Role contact person </div>
+    <div onclick="window.location.href = '/traitclass'" class="MenuButtonLeft <?php echo $Selectedtraitclass; ?>"> Trait class </div>
+    <div onclick="window.location.href = '/triallocation'" class="MenuButtonLeft <?php echo $Selectedtriallocation; ?>"> Trial location </div>
+    <div onclick="window.location.href = '/variablesmeasured'" class="MenuButtonLeft <?php echo $Selectedvariablesmeasured; ?>"> Variables measured </div>
+    <div onclick="window.location.href = '/variety'" class="MenuButtonLeft <?php echo $Selectedvariety; ?>"> Variety </div>
 </div>
