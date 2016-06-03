77c04787160634de91438e984cb400a33227bbec
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-06-03 09:28:09 -0500

    Mejorando Estilo V2

--- lib/filter/doctrine/SfGuardUserFormFilter.class.php
+++ lib/filter/doctrine/SfGuardUserFormFilter.class.php
@@ -16,9 +16,7 @@ class SfGuardUserFormFilter extends BaseSfGuardUserFormFilter {
             'last_name' => new sfWidgetFormFilterInput(),
             'email_address' => new sfWidgetFormFilterInput(array('with_empty' => false)),
             'username' => new sfWidgetFormFilterInput(array('with_empty' => false)),
-            'last_login' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate())),
             'is_active' => new sfWidgetFormChoice(array('choices' => array('' => 'yes or no', 1 => 'yes', 0 => 'no'))),
-            'is_super_admin' => new sfWidgetFormChoice(array('choices' => array('' => 'yes or no', 1 => 'yes', 0 => 'no'))),
             'permissions_list' => new sfWidgetFormDoctrineChoice(array('multiple' => true, 'model' => 'sfGuardPermission')),
         ));
 
@@ -27,9 +25,7 @@ class SfGuardUserFormFilter extends BaseSfGuardUserFormFilter {
             'last_name' => new sfValidatorPass(array('required' => false)),
             'email_address' => new sfValidatorPass(array('required' => false)),
             'username' => new sfValidatorPass(array('required' => false)),
-            'last_login' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
             'is_active' => new sfValidatorChoice(array('required' => false, 'choices' => array('', 1, 0))),
-            'is_super_admin' => new sfValidatorChoice(array('required' => false, 'choices' => array('', 1, 0))),
             'permissions_list' => new sfValidatorDoctrineChoice(array('multiple' => true, 'model' => 'sfGuardPermission', 'required' => false)),
         ));
 
