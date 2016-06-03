33d5ca61b46a707a4132d2977fc6c18263bb1e7d
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-06-03 10:30:58 -0500

    Mejorando Estilo V2

---
+++ plugins/sfDoctrineGuardPlugin/modules/sfGuardUser/templates/newSuccess.php
@@ -0,0 +1,6 @@
+<?php use_helper('I18N', 'Date') ?>
+<?php include_partial('sfGuardUser/assets') ?>
+<?php include_partial('sfGuardUser/flashes') ?>
+<?php include_partial('sfGuardUser/form_header', array('sf_guard_user' => $sf_guard_user, 'form' => $form, 'configuration' => $configuration)) ?>
+<?php include_partial('sfGuardUser/form', array('sf_guard_user' => $sf_guard_user, 'form' => $form, 'configuration' => $configuration, 'helper' => $helper)) ?>
+<?php include_partial('sfGuardUser/form_footer', array('sf_guard_user' => $sf_guard_user, 'form' => $form, 'configuration' => $configuration)) ?>
