61f294f690eb198678e08a40c14ac5dbc48417c2
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-06-03 10:53:05 -0500

    Mejorando Estilo V2

---
+++ plugins/sfDoctrineGuardPlugin/modules/sfGuardUser/templates/_form_fieldset.php
@@ -0,0 +1,16 @@
+<fieldset>
+    <?php foreach ($fields as $name => $field): ?>
+        <?php if ((isset($form[$name]) && $form[$name]->isHidden()) || (!isset($form[$name]) && $field->isReal())) continue ?>
+        <?php
+        include_partial('sfGuardUser/form_field', array(
+            'name' => $name,
+            'attributes' => $field->getConfig('attributes', array()),
+            'label' => $field->getConfig('label'),
+            'help' => $field->getConfig('help'),
+            'form' => $form,
+            'field' => $field,
+            'class' => 'control-type-' . strtolower($field->getType()) . ' control-name-' . $name,
+        ))
+        ?>
+<?php endforeach; ?>
+</fieldset>
