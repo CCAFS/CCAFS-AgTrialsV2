de6b2621304f1f169ff1ff7e6eaae9fd2678e19a
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-09-28 15:23:34 -0500

    Mejorando Estilo V2

--- web/js/introJs/newTrial.js
+++ web/js/introJs/newTrial.js
@@ -1,82 +1,82 @@
-function startIntro(){
-  var intro = introJs();
-  intro.setOptions({
-    steps: [
-      {
-        element: '#nameofproject-block',
-        intro: 'The name that the data provider gives to identify the project'
-      },
-      {
-        element: '#projectleader-block',
-        intro: 'projectleader'
-      },
-      {
-        element: '#projectinstitutions-block',
-        intro: 'projectinstitutions'
-      },
-      {
-        element: '#projectperiod-block',
-        intro: 'projectperiod'
-      },
-      {
-        element: '#projectfunding-block',
-        intro: 'projectfunding'
-      },
-      {
-        element: '#projectinformation-block',
-        intro: 'projectinformation'
-      },
-      {
-        element: '#projecttrialmanager-block',
-        intro: 'projecttrialmanager'
-      },
-      {
-        element: '#projecttrialperiod-block',
-        intro: 'projecttrialperiod'
-      },
-      {
-        element: '#projecttriallocation-block',
-        intro: 'projecttriallocation'
-      },
-      {
-        element: '#projecttrialchar-block',
-        intro: 'projecttrialchar'
-      },
-      {
-        element: '#projectaccessinfo-block',
-        intro: 'projectaccessinfo'
-      },
-      {
-        element: '#projectlicense-block',
-        intro: 'projectlicense'
-      },
-      {
-        element: '#DivCrop1 .cropInfo',
-        intro: 'DivCrop1'
-      },
-      {
-        element: '#DivCrop1 .varieties',
-        intro: 'DivCrop1'
-      },
-      {
-        element: '#DivCrop1 .variablesMeasured',
-        intro: 'DivCrop1'
-      },
-      {
-        element: '#DivCrop1 .dataInformation',
-        intro: '<iframe width="420" height="315" src="https://www.youtube.com/embed/wzfm3ltrixk?t=7m47s" frameborder="0" allowfullscreen></iframe>'
-      },
-      {
-        element: '#nuevocrop',
-        intro: 'nuevocrop'
-      },
-      {
-        element: '#buttons-block',
-        intro: "4"
-      }
-    ]
-  });
-  intro.start();
+function startIntro() {
+    var intro = introJs();
+    intro.setOptions({
+        steps: [
+            {
+                element: '#nameofproject-block',
+                intro: 'The name that the data provider gives to identify the project.'
+            },
+            {
+                element: '#projectleader-block',
+                intro: 'The name and contact information of the person who leads the project.'
+            },
+            {
+                element: '#projectinstitutions-block',
+                intro: 'The name of the institution and the country where it is located of the organization that is implementing the project.'
+            },
+            {
+                element: '#projectperiod-block',
+                intro: 'The known or expected start and end dates of the project.'
+            },
+            {
+                element: '#projectfunding-block',
+                intro: 'The name of the donor organization who is funding the project.'
+            },
+            {
+                element: '#projectinformation-block',
+                intro: 'A short summary of the project in one paragraph and keywords that help identify the focus of the project and its location.'
+            },
+            {
+                element: '#projecttrialmanager-block',
+                intro: 'The name and contact of the person responsible for carrying out the trial. This may be the manager of the experiment station or the principal investigator of the project.'
+            },
+            {
+                element: '#projecttrialperiod-block',
+                intro: 'The beginning and ending dates of all trial activities, or the sowing date and the harvest date for crop trials,'
+            },
+            {
+                element: '#projecttriallocation-block',
+                intro: 'The name and location of the experiment station or trial site, as well as latitude and longitude coordinates and elevation of the site.'
+            },
+            {
+                element: '#projecttrialchar-block',
+                intro: 'A name that you get to the trial and the objectives of the trial.'
+            },
+            {
+                element: '#projectaccessinfo-block',
+                intro: 'As a data provider, you specify the AgTrials registered users  who have access to the data, or whether it can be in the public domain.'
+            },
+            {
+                element: '#projectlicense-block',
+                intro: 'Computer code generated from the Creative Commons License Generator (button on the lower right) that specifies the conditions under which the data can be used.'
+            },
+            {
+                element: '#DivCrop1 .cropInfo',
+                intro: 'Here you indicate information on the design of the trial, including planting, maturity and harvest dates.'
+            },
+            {
+                element: '#DivCrop1 .varieties',
+                intro: 'Here you search for existing variety names or indicate new variety names.'
+            },
+            {
+                element: '#DivCrop1 .variablesMeasured',
+                intro: 'Here you traits measured in the trial, from the Crop Ontology and AgTrials databases.'
+            },
+            {
+                element: '#DivCrop1 .dataInformation',
+                intro: 'This is the upload page where you can upload formatted data to AgTrials.'
+            },
+            {
+                element: '#nuevocrop',
+                intro: 'Here you can add additional crops in the cases of inter-cropping or other types of trials that imply more than one crop.'
+            },
+            {
+                element: '#buttons-block',
+                intro: "4"
+            }
+        ]
+    });
+    intro.start();
 }
 
 
