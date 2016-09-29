76b4222d437acbae414b0a5d4cdd60bb35795811
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-09-29 14:18:31 -0500

    Mejorando Estilo V2

--- apps/agtrials/templates/layout.php
+++ apps/agtrials/templates/layout.php
@@ -63,7 +63,21 @@ if ($sf_user->isAuthenticated()) {
         <?php include_stylesheets(); ?>
         <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'/>
         <!-- Add IntroJs styles -->
-        <link href="/intro.js-2.2.0/minified/introjs.min.css" rel="stylesheet">
+        <link href="/intro.js-2.2.0/minified/introjs.min.css" rel="stylesheet"/>
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-22429807-1']);
+            _gaq.push(['_trackPageview']);
+
+            (function () {
+                var ga = document.createElement('script');
+                ga.type = 'text/javascript';
+                ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
     </head>
     <body> 
         <header >
