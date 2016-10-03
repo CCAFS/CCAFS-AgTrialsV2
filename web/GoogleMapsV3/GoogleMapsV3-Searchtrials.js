316d7ec382649ddd6603ebb2cb816a52913c5cb3
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-10-03 07:31:34 -0500

    Mejorando Estilo V2

--- web/GoogleMapsV3/GoogleMapsV3-Searchtrials.js
+++ web/GoogleMapsV3/GoogleMapsV3-Searchtrials.js
@@ -105,7 +105,7 @@ function Legend(controlDiv) {
             '<span><img width="16" height="16" src="/GoogleMapsV3/images/m2.png"/> 11 - 100</span><br/>' +
             '<span><img width="16" height="16" src="/GoogleMapsV3/images/m3.png"/> 101 - 1,000</span><br/>' +
             '<span><img width="16" height="16" src="/GoogleMapsV3/images/m4.png"/> 1,001 - 10,000</span><br/>' +
-            '<span><img width="16" height="16" src="/GoogleMapsV3/images/m5.png"/> 10,001 - more</span><br/>';
+            '<span><img width="16" height="16" src="/GoogleMapsV3/images/m5.png"/> 10,001 - More</span><br/>';
     controlUI.appendChild(controlText);
 }
 
