d53de136e3c0f755d8cc8fc34a9e633bb003b81f
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-10-31 07:48:21 -0500

    Mejorando Estilo V2

--- apps/agtrials/modules/trial/actions/actions.class.php
+++ apps/agtrials/modules/trial/actions/actions.class.php
@@ -1516,26 +1516,91 @@ class trialActions extends autoTrialActions {
         $output = "$UploadDir/AgTriasData.zip";
 
         $zip = new ZipArchive();
-        $zip->open($output, ZIPARCHIVE::CREATE);
-        $all = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($folder));
 
-        foreach ($all as $f => $value) {
-            echo "F: $f <br>";
-            $zip->addFile($f, $f);
-        }
-        die();
-        $zip->close();
+        /* directorio a comprimir
+         * la barra inclinada al final es importante
+         * la ruta debe ser relativa no absoluta
+         */
+        $dir = $folder;
 
-        if (file_exists($output)) {
-            header('Content-type: "application/zip"');
-            header('Content-Disposition: attachment; filename="AgTriasData.zip"');
-            readfile($output);
-            unlink($output);
+//ruta donde guardar los archivos zip, ya debe existir
+        $rutaFinal = $UploadDir;
+
+        if (!file_exists($rutaFinal)) {
+            mkdir($rutaFinal);
         }
 
-        //unlink($filename);
-        //DeleteDirectory($TmpDir);
-        die();
+        $archivoZip = $output;
+
+        if ($zip->open($archivoZip, ZIPARCHIVE::CREATE) === true) {
+            agregar_zip($dir, $zip);
+            $zip->close();
+
+            //Muevo el archivo a una ruta
+            //donde no se mezcle los zip con los demas archivos
+            rename($archivoZip, "$rutaFinal/$archivoZip");
+
+            //Hasta aqui el archivo zip ya esta creado
+            //Verifico si el archivo ha sido creado
+            if (file_exists($rutaFinal . "/" . $archivoZip)) {
+                echo "Proceso Finalizado!! <br/><br/>
+                Descargar: <a href='$rutaFinal/$archivoZip'>$archivoZip</a>";
+            } else {
+                echo "Error, archivo zip no ha sido creado!!";
+            }
+        }
+
+//        $zip = new ZipArchive();
+//        $zip->open($output, ZIPARCHIVE::CREATE);
+//        $all = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($folder));
+//
+//        foreach ($all as $f => $value) {
+//            echo "F: $f <br>";
+//            $zip->addFile($f, $f);
+//        }
+//        die();
+//        $zip->close();
+//
+//        if (file_exists($output)) {
+//            header('Content-type: "application/zip"');
+//            header('Content-Disposition: attachment; filename="AgTriasData.zip"');
+//            readfile($output);
+//            unlink($output);
+//        }
+//
+//        //unlink($filename);
+//        //DeleteDirectory($TmpDir);
+//        die();
     }
 
+}
+
+function agregar_zip($dir, $zip) {
+    //verificamos si $dir es un directorio
+    if (is_dir($dir)) {
+        //abrimos el directorio y lo asignamos a $da
+        if ($da = opendir($dir)) {
+            //leemos del directorio hasta que termine
+            while (($archivo = readdir($da)) !== false) {
+                /* Si es un directorio imprimimos la ruta
+                 * y llamamos recursivamente esta función
+                 * para que verifique dentro del nuevo directorio
+                 * por mas directorios o archivos
+                 */
+                if (is_dir($dir . $archivo) && $archivo != "." && $archivo != "..") {
+                    echo "<strong>Creando directorio: $dir$archivo</strong><br/>";
+                    agregar_zip($dir . $archivo . "/", $zip);
+
+                    /* si encuentra un archivo imprimimos la ruta donde se encuentra
+                     * y agregamos el archivo al zip junto con su ruta 
+                     */
+                } elseif (is_file($dir . $archivo) && $archivo != "." && $archivo != "..") {
+                    echo "Agregando archivo: $dir$archivo <br/>";
+                    $zip->addFile($dir . $archivo, $dir . $archivo);
+                }
+            }
+            //cerramos el directorio abierto en el momento
+            closedir($da);
+        }
+    }
 }
