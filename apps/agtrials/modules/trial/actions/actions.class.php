0eb46bb63117c5c8b9f867dafebd1450eb7e2cb9
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-10-31 11:11:22 -0500

    Mejorando Estilo V2

--- apps/agtrials/modules/trial/actions/actions.class.php
+++ apps/agtrials/modules/trial/actions/actions.class.php
@@ -1513,8 +1513,6 @@ class trialActions extends autoTrialActions {
         $DirFiles = $TmpDownloaddataDir . "/";
         $FileZip = "$UploadDir/AgTrialsData.zip";
         $DirBase = "AgTrialsData";
-        
-        die(".");
 
         $Zip = new ZipArchive();
         if ($Zip->open($FileZip, ZIPARCHIVE::CREATE) === true) {
