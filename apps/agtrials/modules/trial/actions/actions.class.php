3fe483dc1c2498d59122adc62b4599db7d60a042
hrespinosa <hrespinosa@CIATCO00135L.CGIARAD.ORG>
2016-10-31 10:28:59 -0500

    Mejorando Estilo V2

--- apps/agtrials/modules/trial/actions/actions.class.php
+++ apps/agtrials/modules/trial/actions/actions.class.php
@@ -1532,55 +1532,17 @@ class trialActions extends autoTrialActions {
 }
 
 function agregar_zip($Dirbase, $dir, $zip) {
-    //verificamos si $dir es un directorio
     if (is_dir($dir)) {
-        //abrimos el directorio y lo asignamos a $da
         $da = opendir($dir);
         if ($da) {
-            //leemos del directorio hasta que termine
             while (($archivo = readdir($da)) !== false) {
-
                 if (is_dir($dir . $archivo) && $archivo != "." && $archivo != "..") {
-                    $NewDir = "/" . strstr($dir . $archivo, $Dirbase) . "/";
-
-                    echo "<strong>Creando directorio: $dir$archivo</strong><br/>";
                     agregar_zip($Dirbase, $dir . $archivo . "/", $zip);
                 } elseif (is_file($dir . $archivo) && $archivo != "." && $archivo != "..") {
                     $File = strstr($dir . $archivo, $Dirbase);
-                    echo "Agregando archivo: $dir$archivo <br/>";
                     $zip->addFile($dir . $archivo, $File);
                 }
             }
-            closedir($da);
-        }
-    }
-}
-
-function agregar_zipxxx($dir, $zip) {
-    //verificamos si $dir es un directorio
-    if (is_dir($dir)) {
-        //abrimos el directorio y lo asignamos a $da
-        if ($da = opendir($dir)) {
-            //leemos del directorio hasta que termine
-            while (($archivo = readdir($da)) !== false) {
-                /* Si es un directorio imprimimos la ruta
-                 * y llamamos recursivamente esta función
-                 * para que verifique dentro del nuevo directorio
-                 * por mas directorios o archivos
-                 */
-                if (is_dir($dir . $archivo) && $archivo != "." && $archivo != "..") {
-                    echo "<strong>Creando directorio: $dir$archivo</strong><br/>";
-                    agregar_zip($dir . $archivo . "/", $zip);
-
-                    /* si encuentra un archivo imprimimos la ruta donde se encuentra
-                     * y agregamos el archivo al zip junto con su ruta 
-                     */
-                } elseif (is_file($dir . $archivo) && $archivo != "." && $archivo != "..") {
-                    echo "Agregando archivo: $dir$archivo <br/>";
-                    $zip->addFile($dir . $archivo, $dir . $archivo);
-                }
-            }
-            //cerramos el directorio abierto en el momento
             closedir($da);
         }
     }
